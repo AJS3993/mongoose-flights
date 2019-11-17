66e69b0c8b652468c7883358790583bed6babd4f
Andrew Smith <ajs3993@gmail.com>
2019-11-16 18:18:26 -0600

    it works

--- routes/flights.js
+++ routes/flights.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
-/* GET /movies/new */
+/* GET /flights/new */
 router.get('/', flightsCtrl.index);
 router.get('/new', flightsCtrl.new);
 router.post('/', flightsCtrl.create);
