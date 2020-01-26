50ad355a564ed8207cc88fe1267b7baa6a471f33
Andrew Smith <ajs3993@gmail.com>
2020-01-26 14:25:47 -0600

    mongodb connected

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
-)
-
-var db = mongoose.connection;
-
-db.on('connected', function() {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
+// database connection event
+mongoose.connection.on('connected', function () {
+  console.log(`Mongoose connected`);
+});
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./config/database');
 var session = require('express-session');
-
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
 var destinationsRouter = require('./routes/destinations');
@@ -15,6 +13,7 @@ var methodOverride = require('method-override');
 require('dotenv').config();
 
 var app = express();
+require('./config/database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
